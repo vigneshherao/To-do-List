5a3108e74b9439bf0af1907c34ccdcc92b85aec5
vignesh <vigneshfornavy@gmail.com>
2023-10-01 18:11:00 +0530

    todo list

--- app.js
+++ app.js
@@ -9,6 +9,31 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputTask.value;
         list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputTask.value = "";
+    saveData();
 }
+
+list.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } 
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data",list.innerHTML);
+}
+
+function showData(){
+    list.innerHTML = localStorage.getItem("data");
+}
+showData();
